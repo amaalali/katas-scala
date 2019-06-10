2c768e8aa1c81a921133d320b4b32e1e6416e2ab
Amaal Ali <i.ama.dev@outlook.com>
2019-06-10 22:53:24 +0100

    Refactor Fizzbuzz.

--- src/main/scala/kata/fizzbuzz/Fizzbuzz.scala
+++ src/main/scala/kata/fizzbuzz/Fizzbuzz.scala
@@ -2,26 +2,21 @@ package kata.fizzbuzz
 
 object FizzBuzz {
   def apply(input: Int): String = {
-      (unwrapper compose sevens compose buzzer compose fizzer)(input, None)
+      (unwrap compose sevens compose buzzer compose fizzer)(input, None)
   }
 
-  private val fizzer = (bla("fizz", isDivBy3, _: Int, _: Option[String])).tupled
-  private val buzzer = (bla("buzz", isDivBy5, _: Int, _: Option[String])).tupled
-  private val sevens = (bla("seven", (input: Int) => input == 7, _: Int, _: Option[String])).tupled
-  private val unwrapper = (unwrap _).tupled
+  private val fizzer = bla("fizz", divBy(3)) _ tupled
+  private val buzzer = bla("buzz", divBy(5)) _ tupled
+  private val sevens = bla("seven", _ == 7) _ tupled
 
-  private def bla(response: String, testFn: Int => Boolean, dividend: Int, responseAcc: Option[String]): (Int, Option[String]) = {
+  private def bla(response: String, testFn: Int => Boolean)( dividend: Int, responseAcc: Option[String]): (Int, Option[String]) = {
     val returnVal = if (testFn(dividend)) Some(responseAcc.getOrElse("") + response) else responseAcc
     (dividend, returnVal)
   }
 
-  private def unwrap(inputValue:Int, responseStr: Option[String]): String = {
-    responseStr.getOrElse(inputValue.toString)
+  private val unwrap: ((Int, Option[String])) => String = {
+    case (inputValue, responseStr) => responseStr.getOrElse(inputValue.toString)
   }
 
-  private val isDivBy3 = divBy(_: Int ,3)
-
-  private val isDivBy5 = divBy(_:Int, 5)
-
-  private def divBy(dividend: Int, divisor: Int): Boolean = dividend % divisor == 0
+  private def divBy(divisor: Int)(dividend: Int): Boolean = dividend % divisor == 0
 }
